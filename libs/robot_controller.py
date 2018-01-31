5556639b24f2008b42a541ef0d9a8b0790d2f419
zhangj8 <zhangj8@rose-hulman.edu>
2018-01-31 09:48:26 -0500

    m3

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -23,11 +23,13 @@ class Snatch3r(object):
         self.left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
         self.right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
 
-    def drive_inches(self, inches, speed):
+    def drive_inches(self, inches_target, speed_deg_per_second):
         # Check that the motors are actually connected
         assert self.left_motor.connected
         assert self.right_motor.connected
 
+        inches = inches_target
+        speed = speed_deg_per_second
         self.left_motor.run_to_rel_pos(position_sp=inches * 90, speed_sp=speed, stop_action=ev3.Motor.STOP_ACTION_BRAKE)
         self.right_motor.run_to_rel_pos(position_sp=inches * 90, speed_sp=speed, stop_action=ev3.Motor.STOP_ACTION_BRAKE)
         self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
